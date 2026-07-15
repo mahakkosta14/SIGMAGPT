f58cb52ed78eb2ff573fb0d35ab12f5000ee134f
Mahak kosta <mahakkosta014@gmail.com>
2026-07-15 19:04:00 +0530

    Updated API URL to use environment variable

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -30,7 +30,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/chat, options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
